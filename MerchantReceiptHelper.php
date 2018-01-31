b1fa014ba953166e2af9ea48c06aa16ed24e49dc
Sergey Zhukov <zhukovsm@mail.ru>
2018-01-31 15:00:07 +0300

    task 4
    divided merchantReceipt by Item and MerchantReceiptHelper

--- MerchantReceiptHelper.php
+++ MerchantReceiptHelper.php
@@ -1,17 +1,5 @@
 <?php
 namespace MerhantReceipt;
-class Item{
-    public $price;
-    public $quantity;
-    public $title;
-    public $tax;
-    public function __construct($base_price, $quantity, $title, $tax, $discount) {
-        $this->price = $base_price - round($base_price * $discount, 2);
-        $this->quantity = $quantity;
-        $this->tax = $tax;
-        $this->title = $title;
-    }
-}
 
 class MerchantReceiptHelper{
     private $common_price=0;
