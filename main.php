0be4cca60c87071d60dcb03807a8d7125ee74d71
Sergey Zhukov <zhukovsm@mail.ru>
2018-02-13 13:17:08 +0300

    тестовый файл
    Здесь буду тестировать

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+namespace MerchantReceipt;// as merchantReceipt;
+require "MerchantReceiptHelper.php";
+//$item = new Item("", "", "", "");
+$mreceipt = new MerchantReceiptHelper(10, 32423423424, "s.zhukov@intellectmoney.ru", "Main");
+
